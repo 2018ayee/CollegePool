65de13b0643173269ee036ec4eefb41d412f197f
Adam <adam.xyken@108.48.109.136>
2018-09-29 22:05:27 -0400

    Created forms for posting offers/requests

--- components/home/home_page.js
+++ components/home/home_page.js
@@ -1,4 +1,30 @@
 document.getElementById("defaultTab").click();
+document.getElementById("defaultModalTab").click();
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("Post");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 function changeTab(evt, tab){
 	// Declare all variables
@@ -19,4 +45,29 @@ function changeTab(evt, tab){
     // Show the current tab, and add an "active" class to the link that opened the tab
     document.getElementById(tab).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+function changeModalTab(evt, tab){
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("modaltabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("modaltablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(tab).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function submitOffer(){
+    span.click();
 }
